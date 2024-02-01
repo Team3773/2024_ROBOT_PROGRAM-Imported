fc92954acae78016333fb09e78c9a6383bfdcae3
Garrett Ruffner <garrettruffner42@gmail.com>
2024-01-31 21:44:58 -0600

    Add Motor controllers as followers

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -5,35 +5,32 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Robot;
-
-import javax.management.loading.PrivateClassLoader;
-
-//import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.Talon;
 
+public class DriveSubsystem extends SubsystemBase {
 
-public class DriveSubsystem<TalonSRX> extends SubsystemBase {
+  private static final double kMaxSpeed = 3.0; // meters per second
+  private static final double kMaxAngularSpeed = 2 * Math.PI; // one rotation
+  private WPI_TalonSRX m_leftMotor;
+  private WPI_TalonSRX m_rightMotor;
+  private WPI_TalonSRX m_leftMotorFollower;
+  private WPI_TalonSRX m_rightMotorFollower;
+  private DifferentialDrive drive;
 
-  public static final double kMaxSpeed = 3.0; // meters per second
-  public static final double kMaxAngularSpeed = 2 * Math.PI; // one rotation
-  //per second
-
-  
-  //private static final double kTrackWidth = 0.381 * 2; // meters
-  //private static final double kWheelRadius = 0.0508; // meters
-  //private static final int kEncoderResolution = 4096;
+  // private static final double kTrackWidth = 0.381 * 2; // meters
+  // private static final double kWheelRadius = 0.0508; // meters
+  // private static final int kEncoderResolution = 4096;
   /** Creates a new DriveTrain. */
-  
-  private DriveSubsystem() {
-    final Talon m_leftMotor = new Talon(RobotMap.m_leftMotorPort);
-    final Talon m_rightMotor = new Talon(RobotMap.m_rightMotorPort);
-    final Talon m_leftMotorFollower = new Talon(RobotMap.m_leftMotorFollowerPort);
-    final Talon m_rightMotorFollower = new Talon(RobotMap.m_rightMotorFollowerPort);
-    
-  final DifferentialDrive drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+  public DriveSubsystem() {
+    m_leftMotor = new WPI_TalonSRX(RobotMap.m_leftMotorPort);
+    m_rightMotor = new WPI_TalonSRX(RobotMap.m_rightMotorPort);
+    m_leftMotorFollower = new WPI_TalonSRX(RobotMap.m_leftMotorFollowerPort);
+    m_rightMotorFollower = new WPI_TalonSRX(RobotMap.m_rightMotorFollowerPort);
+    m_leftMotorFollower.follow(m_leftMotor);
+    m_rightMotorFollower.follow(m_rightMotor);
+    drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   }
 
   public void arcadeDrive(double speed, double rotation) {
