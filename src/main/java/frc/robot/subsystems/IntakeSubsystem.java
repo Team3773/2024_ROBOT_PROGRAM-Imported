283ec256ada42aa18015d196dc706bbe13548dda
Garrett Ruffner <garrettruffner42@gmail.com>
2024-02-15 09:18:51 -0600

    Remove Base Class From Intake Subsystem

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,43 +1,25 @@
 package frc.robot.subsystems;
 
-
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkBase;
-import com.revrobotics.CANSparkLowLevel.MotorType; 
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new IntakeSubsystem. */
- 
+
   private CANSparkMax m_armMotor;
-  private CANSparkMax m_noteLock;
-  //com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX m_armMotor = new WPI_TalonSRX(RobotMap.m_armMotorPort); change motor contgroller to SparkMax for neo 1.1 motor
-  
+  private static final double speedModifier = .5;
+  // com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX m_armMotor = new
+  // WPI_TalonSRX(RobotMap.m_armMotorPort); change motor contgroller to SparkMax
+  // for neo 1.1 motor
 
   public IntakeSubsystem() {
-
-  m_armMotor = new CANSparkMax (RobotMap.m_armMotorPort,MotorType.kBrushless);
+    m_armMotor = new CANSparkMax(RobotMap.m_armMotorPort, MotorType.kBrushless);
     m_armMotor.setInverted(false);
-  //m_noteLock = new CANSparkMax (RobotMap.m_noteLockPort,MotorType.kBrushless);
-  // Create a new CANSparkBase object with device ID 4
-CANSparkBase spark = new CANSparkBase (RobotMap.m_armMotorPort, MotorType.kBrushless);
-
-// Set the idle mode to brake
-spark.setIdleMode(CANSparkBase.IdleMode.kBrake);
-
-// Set the output to 0.5 (50% duty cycle)
-spark.set(0.5);
-
-// Wait for 2 seconds
-Timer.delay(2);
-
-// Set the output to 0 (stop the motor)
-spark.set(0);
-
-// The motor will brake and resist any external motion
-}
-
+    // Set the idle mode to brake
+    m_armMotor.setIdleMode(CANSparkMax.IdleMode.kBrake);
+    // The motor will brake and resist any external motion
+  }
 
   @Override
   public void periodic() {
@@ -45,8 +27,7 @@ spark.set(0);
   }
 
   public void runIntake(double speed) {
-    System.out.println("Intake speed:" + speed);
-    double speedModifier = .5;
+    System.out.println("Intake speed:" + speed * speedModifier);    
     m_armMotor.set(speed * speedModifier);
   }
 
