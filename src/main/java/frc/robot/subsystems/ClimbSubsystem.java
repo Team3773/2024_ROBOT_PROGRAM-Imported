d4232eeeb541f43fca88e892c91aed0014a23bb2
Team3773 <77553727+Team3773@users.noreply.github.com>
2024-02-17 11:28:04 -0500

    Added second climb motor

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -5,16 +5,20 @@ import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
-  private CANSparkMax m_liftMotor;
+  private CANSparkMax m_leftLiftMotor;
+  private CANSparkMax m_rightLiftMotor;
     
-  public ClimbSubsystem(CANSparkMax m_liftMotor) {
-    this.m_liftMotor = m_liftMotor;
+  public ClimbSubsystem(CANSparkMax m_leftLiftMotor, CANSparkMax m_rightLiftMotor) {
+    this.m_leftLiftMotor = m_leftLiftMotor;
+    this.m_rightLiftMotor = m_rightLiftMotor;
   }
 
   public ClimbSubsystem() {
 
-  m_liftMotor = new CANSparkMax (RobotMap.m_liftMotorPort,MotorType.kBrushless);
-    m_liftMotor.setInverted(false);
+  m_leftLiftMotor = new CANSparkMax (RobotMap.m_leftLiftMotorPort,MotorType.kBrushless);
+    m_leftLiftMotor.setInverted(false);
+  m_rightLiftMotor = new CANSparkMax (RobotMap.m_rightLiftMotorPort,MotorType.kBrushless);
+    m_rightLiftMotor.setInverted(true);
   }
 
   @Override
@@ -24,13 +28,15 @@ public class ClimbSubsystem extends SubsystemBase {
 
   public void runLift(double speed) {
     System.out.println("Lift speed:" + speed);
-    m_liftMotor.set(speed);
+    m_leftLiftMotor.set(speed);
+    m_rightLiftMotor.set(speed);
   }
 
   public void stopLift() {
     var speed = 0;
     System.out.println("Lift speed:" + speed);
-    m_liftMotor.set(speed);
+    m_leftLiftMotor.set(speed);
+    m_rightLiftMotor.set(speed);
   }
 
 }
--- src/main/java/frc/robot/subsystems/RobotMap.java
+++ src/main/java/frc/robot/subsystems/RobotMap.java
@@ -6,8 +6,8 @@ public class RobotMap {
     final static int m_leftMotorFollowerPort = 1;
     final static int m_rightMotorFollowerPort = 3;
     final static int m_armMotorPort = 4;
-    public final static int m_noteLockPort = 5;
-    public final static int m_liftMotorPort = 6;
+    public final static int m_leftLiftMotorPort = 6;
+    public final static int m_rightLiftMotorPort = 7;
 //XboxController port works differently due to this number being able to change, meaning it needs to be public
 }
 
